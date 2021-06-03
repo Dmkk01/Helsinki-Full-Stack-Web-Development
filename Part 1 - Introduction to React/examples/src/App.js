ab36f50b2e4de86775efeda3f9f84b8c6a3a0ea8
Dominik <dmkk1@outlook.com>
2021-06-03 15:59:32 +0300

    Upload Examples Part 1 D

--- Part 1 - Introduction to React/examples/src/App.js
+++ Part 1 - Introduction to React/examples/src/App.js
@@ -1,27 +1,50 @@
 import React, { useState } from 'react'
 
-const App = () => {
-  const [ counter, setCounter ] = useState(0)
+// const App = () => {
+//   const [ counter, setCounter ] = useState(0)
 
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
+//   const increaseByOne = () => setCounter(counter + 1)
+//   const decreaseByOne = () => setCounter(counter - 1)
+//   const setToZero = () => setCounter(0)
+
+//   return (
+//     <div>
+//       <Display counter={counter}/>
+//       <Button
+//         handleClick={increaseByOne}
+//         text='plus'
+//       />
+//       <Button
+//         handleClick={setToZero}
+//         text='zero'
+//       />     
+//       <Button
+//         handleClick={decreaseByOne}
+//         text='minus'
+//       />           
+//     </div>
+//   )
+// }
+
+// const Button = ({ handleClick, text }) => (
+//   <button onClick={handleClick}>
+//     {text}
+//   </button>
+// )
+
+// const Display = ({ counter }) => <div>{counter}</div>
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
 
   return (
     <div>
-      <Display counter={counter}/>
-      <Button
-        handleClick={increaseByOne}
-        text='plus'
-      />
-      <Button
-        handleClick={setToZero}
-        text='zero'
-      />     
-      <Button
-        handleClick={decreaseByOne}
-        text='minus'
-      />           
+      button press history: {props.allClicks.join(' ')}
     </div>
   )
 }
@@ -32,6 +55,30 @@ const Button = ({ handleClick, text }) => (
   </button>
 )
 
-const Display = ({ counter }) => <div>{counter}</div>
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
+  return (
+    <div>
+      {left}
+      <Button handleClick={handleLeftClick} text='left' />
+      <Button handleClick={handleRightClick} text='right' />
+      {right}
+      <History allClicks={allClicks} />
+    </div>
+  )
+}
 
 export default App
