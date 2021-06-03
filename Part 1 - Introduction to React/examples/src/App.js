4ff4428906ae20f49702d40efd415479af8c7076
Dominik <dmkk1@outlook.com>
2021-06-03 14:09:42 +0300

    Upload Examples Part 1 C

--- Part 1 - Introduction to React/examples/src/App.js
+++ Part 1 - Introduction to React/examples/src/App.js
@@ -1,27 +1,37 @@
-import React from 'react'
-
-
-const Hello = (props) => {
-  return (
-    <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
-    </div>
-  )
-}
+import React, { useState } from 'react'
 
 const App = () => {
-  const name = 'Peter'
-  const age = 10
+  const [ counter, setCounter ] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
 
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
+      <Display counter={counter}/>
+      <Button
+        handleClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        handleClick={setToZero}
+        text='zero'
+      />     
+      <Button
+        handleClick={decreaseByOne}
+        text='minus'
+      />           
     </div>
   )
 }
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const Display = ({ counter }) => <div>{counter}</div>
 
 export default App
