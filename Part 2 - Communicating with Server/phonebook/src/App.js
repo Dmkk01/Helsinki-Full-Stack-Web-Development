37b89e086cd2d1610a4ac9d5d851f16bc9977434
Dominik <dmkk1@outlook.com>
2021-06-06 23:32:50 +0300

    Upload Exercise 2.9

--- Part 2 - Communicating with Server/phonebook/src/App.js
+++ Part 2 - Communicating with Server/phonebook/src/App.js
@@ -15,13 +15,20 @@ const App = () => {
 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [filter, setFilter] = useState('')
 
   const handlePersonChange = (event) => {
     setNewName(event.target.value)
   }
+
   const handlePhoneChange = (event) => {
     setNewNumber(event.target.value)
   }
+
+  const handleFilter = (event) => {
+    setFilter(event.target.value)
+  }
+
   const addPerson = (event) => {
     event.preventDefault()
     let check = persons.filter( person => (person.name === newName))
@@ -36,10 +43,25 @@ const App = () => {
       setPersons(persons.concat(personObject))
     }
     setNewName('')
+    setNewNumber('')
   }
+
+  const personsToShow = () => {
+    if (filter.length === 0) {
+      return persons
+    }
+    else {
+      return persons.filter(person => person.name.includes(filter))
+    }   
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={filter} onChange={handleFilter}/>
+      </div>
+      <h2> Add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handlePersonChange}/>
@@ -52,7 +74,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <Person name={person.name} number={person.number} key={person.name}/>)}
+      {personsToShow().map(person => <Person name={person.name} number={person.number} key={person.name}/>)}
     </div>
   )
 }
