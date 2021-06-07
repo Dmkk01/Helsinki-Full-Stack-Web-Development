bac6d20406d8451540541564ee49d425ab0c1305
Dominik <dmkk1@outlook.com>
2021-06-07 11:02:16 +0300

    Upload Exercise 2.14

--- Part 2 - Communicating with Server/countries/src/App.js
+++ Part 2 - Communicating with Server/countries/src/App.js
@@ -2,16 +2,33 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const CountrySolo = ({countrySolo}) => {
+  // Commented out due to the api monthly limit usage
+  // const [weather, setWeather] = useState([])
+  // let REACT_APP_API_KEY = ...
+  // let api_link = `http://api.weatherstack.com/current?access_key=${REACT_APP_API_KEY}&query=${countrySolo.capital}`
+  // const hook2 = () => {
+  //   axios
+  //     .get(api_link)
+  //     .then(response => {
+  //       setWeather(response.data)
+  //     })
+  // }
+  // useEffect(hook2, [api_link])
+
   return (
     <>
       <h1> {countrySolo.name}</h1>
       <p> Capital: {countrySolo.capital}</p>
       <p> Population: {countrySolo.population}</p>
-      <p> Languages</p>
+      <h2> Spoken Languages</h2>
       <ul>
         {countrySolo.languages.map(lang => <li key={lang.name}> {lang.name} </li>)}
       </ul>
       <img src={countrySolo.flag} alt="" style={{height: '150px'}}/>
+      <h2> Weather in {countrySolo.capital}</h2>
+      {/* <p> Temperature: {weather.current.temperature} Celsius</p>
+      <img src={weather.current.weather_icons[0]} alt="" style={{height: '100px'}}/>
+      <p> Wind: {weather.current.wind_speed} mph direction {weather.current.wind_dir}</p> */}
     </>
   )
 }
@@ -46,6 +63,7 @@ const App = () => {
       })
   }
   useEffect(hook, [])
+
 
   const handleFilterChange = (event) => {
     setFilter(event.target.value)
