68828310e3dfbe379172116637fa9f19ba4d1221
Dominik <dmkk1@outlook.com>
2021-06-13 23:49:25 +0300

    Upload Exercise 4.5

--- Part 4 - Testing Express servers, user administration/blog/utils/list_helper.js
+++ Part 4 - Testing Express servers, user administration/blog/utils/list_helper.js
@@ -1,15 +1,25 @@
 const dummy = (blogs) => {
     const x = 1
     return x
-  }
+}
 
 const totalLikes = (blogs) => {
     return blogs.reduce((total, value) => {
         return total + value.likes
     }, 0)
 }
-  
-  module.exports = {
+
+const favouriteBlog = (blogs) => {
+    const value = blogs.reduce(
+        (max, each) => (each.likes > max ? each.likes : max),
+        blogs[0].likes
+      );
+    const result = blogs.find(x => x.likes === value)
+    return result
+}
+
+module.exports = {
     dummy,
-    totalLikes
-  }
+    totalLikes,
+    favouriteBlog
+}
