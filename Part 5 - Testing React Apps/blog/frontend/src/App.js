aa4296824904c2eddb22001fcc1c557ad442ec0c
Dominik <dmkk1@outlook.com>
2021-06-17 11:40:48 +0300

    Upload Exercise 5.6

--- Part 5 - Testing React Apps/blog/frontend/src/App.js
+++ Part 5 - Testing React Apps/blog/frontend/src/App.js
@@ -12,9 +12,6 @@ const App = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
-  const [newTitle, setNewTitle] = useState('')
-  const [newAuthor, setNewAuthor] = useState('')
-  const [newUrl, setNewUrl] = useState('')
   const [errorMessage, setErrorMessage] = useState(null)
   const blogFormRef = React.createRef()
 
@@ -60,28 +57,6 @@ const App = () => {
     setUser(null)
   }
 
-  const addBlog = (event) => {
-    event.preventDefault()
-    blogFormRef.current.toggleVisibility()
-    const blogObject = {
-      title: newTitle,
-      author: newAuthor,
-      url: newUrl,
-    }
-
-    blogService
-      .create(blogObject)
-      .then(data => {
-        setErrorMessage(`a new blog ${newTitle} by ${newAuthor} added`)
-        setTimeout(() => {
-          setErrorMessage(null)
-        }, 3000)
-        setBlogs(blogs.concat(data))
-        setNewTitle('')
-        setNewAuthor('')
-        setNewUrl('')
-      })
-  }
 
   if (user === null) {
     return (
@@ -103,9 +78,7 @@ const App = () => {
         <button type='button' onClick={handleLogout}>logout</button>
       </p>
       <Togglable buttonLabel="new blog" ref={blogFormRef}>
-        <FormBlog
-          handleAdding={addBlog} handleTitle={({ target }) => setNewTitle(target.value)} handleAuthor={({ target }) => setNewAuthor(target.value)}
-          handleUrl={({ target }) => setNewUrl(target.value)} title={newTitle} author={newAuthor} url={newUrl} blogs={blogs}/>
+        <FormBlog setBlogs={setBlogs} setErrorMessage={setErrorMessage} blogs={blogs} blogFormRef={blogFormRef}/>
       </Togglable>
       {blogs.map(blog => <Blog key={blog.id} blog={blog} />)}
     </div>
--- Part 5 - Testing React Apps/blog/frontend/src/components/FormBlog.js
+++ Part 5 - Testing React Apps/blog/frontend/src/components/FormBlog.js
@@ -1,22 +1,47 @@
-import React from 'react'
+import React, {useState} from 'react'
+import blogService from '../services/blogs'
 
-const BlogForm = ({handleAdding, handleTitle, handleAuthor, handleUrl, title, author, url}) => {
+const BlogForm = ({blogFormRef, setBlogs, setErrorMessage, blogs}) => {
+
+    const [newTitle, setNewTitle] = useState('')
+    const [newAuthor, setNewAuthor] = useState('')
+    const [newUrl, setNewUrl] = useState('')
+
+    const addBlog = (event) => {
+        event.preventDefault()
+        blogFormRef.current.toggleVisibility()
+        const blogObject = {
+          title: newTitle,
+          author: newAuthor,
+          url: newUrl,
+        }
+    
+        blogService
+          .create(blogObject)
+          .then(data => {
+            setErrorMessage(`a new blog ${newTitle} by ${newAuthor} added`)
+            setTimeout(() => {
+              setErrorMessage(null)
+            }, 3000)
+            setBlogs(blogs.concat(data))
+            setNewTitle('')
+            setNewAuthor('')
+            setNewUrl('')
+          })
+      }
 
   return (
     <div>
       <h2>Create a new Blog Post</h2>
-      <form onSubmit={handleAdding}>
+      <form onSubmit={addBlog}>
           <div>
-            title:
-            <input type="text" value={title} name="Title" onChange={handleTitle}/>
+            title: <input type="text" value={newTitle} name="Title" onChange={({ target }) => setNewTitle(target.value)}/>
           </div>
           <div>
-            author:
-            <input type="text" value={author} name="Author" onChange={handleAuthor}/>
+            author: <input type="text" value={newAuthor} name="Author" onChange={({ target }) => setNewAuthor(target.value)}/>
           </div>
           <div>
-            url:
-            <input type="text" value={url} name="URL" onChange={handleUrl}/>
+            url: <input type="text" value={newUrl} name="URL" onChange={({ target }) => setNewUrl(target.value)}/>
           </div>
           <button type="submit">create</button>
         </form>
