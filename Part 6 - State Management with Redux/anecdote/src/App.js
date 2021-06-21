94c05413294208c93bbec7eaeffa83a7e2657b67
Dominik <dmkk1@outlook.com>
2021-06-21 13:33:47 +0300

    Upload Exercise 6.7

--- Part 6 - State Management with Redux/anecdote/src/App.js
+++ Part 6 - State Management with Redux/anecdote/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { addVote, createAnecdote } from './reducers/anecdoteReducer'
+import { addVote } from './reducers/anecdoteReducer'
+import AnecdoteForm from './components/AnecdoteForm'
 
 const App = () => {
   const anecdotes = useSelector(state => state)
@@ -8,12 +9,6 @@ const App = () => {
 
   const vote = (id) => {
     console.log('vote', id)
-  }
-  const addAnecdote = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    dispatch(createAnecdote(content))
   }
   return (
     <div>
@@ -30,10 +25,7 @@ const App = () => {
         </div>
       )}
       <h2>create new</h2>
-      <form onSubmit={addAnecdote}>
-        <div><input name="anecdote"/></div>
-        <button type="submit">create</button>
-      </form>
+      <AnecdoteForm/>
     </div>
   )
 }
