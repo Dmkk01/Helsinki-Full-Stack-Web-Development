ffb903cb9104f84a1045e7baad867b699b37cd91
Dominik <dmkk1@outlook.com>
2021-06-29 15:27:06 +0300

    Upload Exercise 8.4

--- Part 8 - GraphQL/library/index.js
+++ Part 8 - GraphQL/library/index.js
@@ -105,7 +105,7 @@ const typeDefs = gql`
   type Query {
       bookCount: Int!
       authorCount: Int!
-      allBooks: [Book!]!
+      allBooks(author: String): [Book!]!
       allAuthors: [Author]
   }
 `
@@ -114,7 +114,14 @@ const resolvers = {
   Query: {
       bookCount: () => books.length,
       authorCount: () => authors.length,
-      allBooks: () => books,
+      allBooks: (root, args) => {
+          if (args.author) {
+              return books.filter(b => b.author === args.author)
+          }
+          else {
+              return books
+          }
+      },
       allAuthors: () => {
         const aut = authors.map((author) => {
             const bookCount = books.reduce((a, book) => (book.author == author.name ? a + 1 : a), 0)
