9f0cebeb60b6ff4243875766083a58c7367d571e
Dominik <dmkk1@outlook.com>
2021-06-29 16:08:12 +0300

    Upload Exercise 8.7

--- Part 8 - GraphQL/library/index.js
+++ Part 8 - GraphQL/library/index.js
@@ -117,6 +117,7 @@ const typeDefs = gql`
       published: Int!
       genres: [String!]!
     ): Book
+    editAuthor(name: String!, setBornTo: Int!): Author
   }
 `
 
@@ -151,6 +152,13 @@ const resolvers = {
         const book = {...args, id: uuid()}
         books = books.concat(book)
         return book
+      },
+      editAuthor: (root, args) => {
+        const author = authors.find(x => x.name === args.name)
+        if (!author) return null
+        const updatedAuthor = { ...author, born: args.setBornTo }
+        authors = authors.map(author => author.name === updatedAuthor.name ? updatedAuthor : author)
+        return updatedAuthor
       }
   }
 }
