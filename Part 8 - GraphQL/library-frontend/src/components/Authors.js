47fef1feb1804e5201151f1ad113198f9407aee7
Dominik <dmkk1@outlook.com>
2021-06-30 12:55:19 +0300

    Upload Exercise 8.11 8.12

--- Part 8 - GraphQL/library-frontend/src/components/Authors.js
+++ Part 8 - GraphQL/library-frontend/src/components/Authors.js
@@ -1,10 +1,22 @@
 import React from 'react'
-import { useQuery } from '@apollo/client';
-import { ALL_AUTHORS } from '../queries'
+import { useQuery, useMutation } from '@apollo/client';
+import { ALL_AUTHORS, EDIT_AUTHOR } from '../queries'
 
 const Authors = (props) => {
   const result = useQuery(ALL_AUTHORS)
+  const [updateAuthor] = useMutation(EDIT_AUTHOR, {
+    refetchQueries: [{ query: ALL_AUTHORS }],
+  })
   let authors = []
+  
+  const submit = (e) => {
+    e.preventDefault()
+    let name = e.target.name.value
+    let setBornTo = parseInt(e.target.born.value)
+    updateAuthor({ variables: { name, setBornTo } })
+    e.target.reset()
+  }
+
   if (!props.show) {
     return null
   }
@@ -36,7 +48,23 @@ const Authors = (props) => {
           )}
         </tbody>
       </table>
-
+      <h2>Set birthyear</h2>
+      <form onSubmit={submit}>
+        <select name="name">
+          {authors.map((a) => {
+            return (
+              <option key={a.id} value={a.name}>
+                {a.name}
+              </option>
+            )
+          })}
+        </select>
+        <div>
+          born
+          <input type="number" name="born" />
+        </div>
+        <button type="submit">Update author</button>
+      </form>
     </div>
   )
 }
